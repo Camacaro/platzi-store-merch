72da64631fb040c2c94bf677c9930424870c593b
Jesus Camacaro <jesus.camacaro@imageimaker.com>
2020-12-23 08:44:07 -0300

    Styles en layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/components/Header.css'
+
+const Header = () => (
+  <div className="Header">
+    <h1> PlatziConf Merch </h1>
+    <div className="Header-Checkout">
+      Checkout
+    </div>
+  </div>
+  )
+
+export default Header
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Layout from '../components/Layout';
 import Checkout from '../container/Checkout';
 import Home from '../container/Home';
 import Information from '../container/Information';
@@ -9,14 +10,16 @@ import Success from '../container/Success';
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/checkout" component={Checkout} />
-      <Route exact path="/checkout/information" component={Information} />
-      <Route exact path="/checkout/payment" component={Payment} />
-      <Route exact path="/checkout/success" component={Success} />
-      <Route component={NotFound} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/checkout/information" component={Information} />
+        <Route exact path="/checkout/payment" component={Payment} />
+        <Route exact path="/checkout/success" component={Success} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 )
 
